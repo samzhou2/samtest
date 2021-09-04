f28357f4f7f6e69ad7c2e117d908a018ba49393e
Sam Zhou <sam.zhou@tigergraph.com>
2021-09-04 21:50:45 +0800

    Update bulkcreate_syd.php

--- bulkcreate_syd.php
+++ bulkcreate_syd.php
@@ -11,27 +11,17 @@ if (isset($_GET['bulk_size']) && $_GET['bulk_size']!="" && is_numeric($_GET['bul
  
 if (isset($_GET['customer_name']) && $_GET['customer_name']!="")
 {
+    mysqli_query($con_read_syd_1, "set aurora_replica_read_consistency = 'session'");
+
     for ($i = 0; $i < $bulk_size; $i++)
     {   
         $customer_name = $_GET['customer_name'].base64_encode(random_bytes(10));
         $query = "INSERT INTO `customers` (`customer_name`,`customer_email`,`customer_contact`,`customer_address`,`country`) values ('$customer_name','samzhou.it@gmail.com','13676042888','Room 1206','China')";
         mysqli_query($con_read_syd_1, $query);
-        $result = mysqli_query($con_read_syd_1, "SELECT * FROM `customers` WHERE customer_id=LAST_INSERT_ID()");
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        
-        $customerData['customer_id'] = $row['customer_id'];
-        $customerData['customer_name'] = $row['customer_name'];
-        $customerData['customer_email'] = $row['customer_email'];
-        $customerData['customer_contact'] = $row['customer_contact'];
-        $customerData['customer_address'] = $row['customer_address'];
-        $customerData['country'] = $row['country'];
-        
-        $customerList[$i] = $customerData;
     }
 
     $response["status"] = "true";
-    $response["message"] = "Customer Details";
-    $response["customers"] = $customerList;
+    $response["message"] = "Customer bulk insert done!";
 }
 else
 {
